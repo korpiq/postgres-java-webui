3aa0768086bd931d027191b9115d76c601d1de20
Kalle Hallivuori <korpiq+claude-host@korpiq.iki.fi>
2026-01-11 16:14:31 +0200

    Claude fixes to renaming app as pogrejab

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,15 +1,15 @@
 -- Initialize database with user having all privileges
 -- This script runs automatically when the container is first created
 
--- Grant all privileges on the database to the appuser
-GRANT ALL PRIVILEGES ON DATABASE appdb TO appuser;
+-- Grant all privileges on the database to pogrejab user
+GRANT ALL PRIVILEGES ON DATABASE pogrejab_db TO pogrejab;
 
 -- Grant all privileges on all tables in public schema
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO appuser;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO appuser;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO appuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pogrejab;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO pogrejab;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO pogrejab;
 
 -- Grant privileges for future tables/sequences/functions
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO appuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO appuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON FUNCTIONS TO appuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO pogrejab;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO pogrejab;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON FUNCTIONS TO pogrejab;
